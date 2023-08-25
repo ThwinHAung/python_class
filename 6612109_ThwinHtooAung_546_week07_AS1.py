0f4beba39e163c4bce4489172e25d381f7ad3570
ThwinHAung <91412490+ThwinHAung@users.noreply.github.com>
2023-08-25 22:05:49 +0700

    Update 6612109_ThwinHtooAung_546_week07_AS1.py

--- 6612109_ThwinHtooAung_546_week07_AS1.py
+++ 6612109_ThwinHtooAung_546_week07_AS1.py
@@ -1,7 +1,7 @@
 input_words =input("Enter words: ").split()
 count=0
 for i in input_words:
-if len(i)>4 and i[0]==i[-1]:
-count+=1
+  if len(i)>4 and i[0]==i[-1]:
+    count+=1
 print("Number of words that meets the
 requirements is: ",count)
